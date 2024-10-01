13cc07d8d57a6f02fa22b39609c526438ea97a21
blue-crack12 <thenulapanapiti219@gmail.com>
2024-10-01 09:19:46 +0530

    Update config.js

--- config.js
+++ config.js
@@ -5,11 +5,11 @@ function convertToBool(text, fault = 'true') {
     return text === fault ? true : false;
 }
 module.exports = {
-SESSION_ID: process.env.SESSION_ID || "enter session id",
-MONGODB: process.env.MONGODB || "mongodb://mongo:GJyeHvVRytNLroiWrQnmdBGBbZSRAqFD@autorack.proxy.rlwy.net:58087",
+SESSION_ID: process.env.SESSION_ID || "ZSEHlbBZ#LxIIp7Cm9UZGn7qUw1MzIqiz5bn1WyR9vBAsSe2hEVs",
+MONGODB: process.env.MONGODB || "mongodb://mongo:wvUTbhtdxdbMGajDDfogpBTSAqTvMgWH@junction.proxy.rlwy.net:24596",
 PREFIX: process.env.PREFIX || ".",
 BOT_NAME: process.env.BOT_NAME || "Thenu-MD",
-BOT_NUMBER: process.env.BOT_NUMBER || "94757096717",
+BOT_NUMBER: process.env.BOT_NUMBER || "94767096711",
 AUTO_AI: process.env.AUTO_AI || "true",
 
 };
